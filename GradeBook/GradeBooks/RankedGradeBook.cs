91652007584f42ce645c0d8bac70614e3a8c2169
pfsoft70 <58772140+pfsoft70@users.noreply.github.com>
2019-12-16 10:15:07 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -40,5 +40,13 @@ namespace GradeBook.GradeBooks
             else
                 base.CalculateStatistics();
         }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count() < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStudentStatistics(name);
+        }
     }
 }
